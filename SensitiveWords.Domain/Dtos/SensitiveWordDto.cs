fd781b8c7155e806ec2fb80f87d92c9a9a26dfc2
Jandre Janse van Vuuren <jvv.jandre@outlook.com>
2025-10-03 11:19:58 +0200

    Added SensitiveWord entity and DTO object to ensure that aligns with our DB structure

---
+++ SensitiveWords.Domain/Dtos/SensitiveWordDto.cs
@@ -0,0 +1,36 @@
+﻿namespace SensitiveWords.Domain.Dtos
+{
+    /// <summary>
+    /// Sensitive word DTO for transferring data
+    /// </summary>
+    public class SensitiveWordDto
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="word"></param>
+        /// <param name="isActive"></param>
+        public SensitiveWordDto(int id, string word, bool isActive)
+        {
+            Id = id;
+            Word = word;
+            IsActive = isActive;
+        }
+
+        /// <summary>
+        /// Record ID for word in DB
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Actual word in string like "FOO"
+        /// </summary>
+        public string Word { get; set; } = null!;
+
+        /// <summary>
+        /// Active status of the word
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
